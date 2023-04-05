ea2c17f7c85918c851dd6325904f9e4dccf3c05d
cherifon <67606136+cherifon@users.noreply.github.com>
2023-04-05 16:06:30 +0100

    Add files via upload

---
+++ Projet/Connection/Connection.js
@@ -0,0 +1,57 @@
+/* ------------------------------------ Click on login and Sign Up to  changue and view the effect
+---------------------------------------
+*/
+
+function cambiar_login() {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
+  document.querySelector('.cont_form_login').style.display = "block";
+  document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+  
+  setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);  
+    
+  setTimeout(function(){    
+  document.querySelector('.cont_form_sign_up').style.display = "none";
+  },200);  
+}
+  
+function cambiar_sign_up(at) {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+  document.querySelector('.cont_form_sign_up').style.display = "block";
+  document.querySelector('.cont_form_login').style.opacity = "0";
+    
+  setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
+  },100);  
+  
+  setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
+  },400);  
+  
+  
+}    
+  
+  
+  
+function ocultar_login_sign_up() {
+  
+  document.querySelector('.cont_forms').className = "cont_forms";  
+  document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+  document.querySelector('.cont_form_login').style.opacity = "0"; 
+  
+  setTimeout(function(){
+  document.querySelector('.cont_form_sign_up').style.display = "none";
+  document.querySelector('.cont_form_login').style.display = "none";
+  },500);  
+    
+}
+  
+  
+  
+function verif(x,y){
+  pass1 = document.getElementById(x).value;
+  pass2 = document.getElementById(y).value;
+  if(pass1 == pass2){
+    alert("Passwords Match");
+  }
+  else{
+    alert("Passwords Do Not Match");
+  }
+}
---
+++ Projet/Page_acceuil/script.js
@@ -0,0 +1,4 @@
+function like() { // Fonction qui permet de liker un post, de changer la couleur du bouton et d'affciher une alerte
+    bouton.style.color = "red";
+    alert("Vous avez liké ce post !");
+}
